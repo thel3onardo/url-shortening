f1cd2933d5b865dc0ff579450f29010fa9617311
= <=>
2021-12-15 23:11:09 -0300

    feat: styles folder removed; navbar component done

---
+++ src/components/navbar.svelte
@@ -0,0 +1,76 @@
+<nav class="nav-container">
+    <div class="nav-container__title-menu">
+        <div class="nav-container__title-menu__logo">
+            <img src="../../static/images/logo.svg" alt="Shortly logo">
+        </div>
+        <ul>
+            <li>
+                <a href="ss">Features</a>
+            </li>
+            <li>
+                <a href="ss">Pricing</a>
+            </li>
+            <li>
+                <a href="w">Resources</a>
+            </li>
+        </ul>
+    </div>
+    <div class="nav-container__buttons">
+        <button>
+            Login
+        </button>
+        <button>
+            Sign Up
+        </button>
+    </div>
+</nav>
+
+<style lang="sass">
+    .nav-container
+        margin: 0 auto
+        padding: 2em 0
+        max-width: 1200px
+        
+        display: flex
+        justify-content: space-between
+
+    .nav-container__title-menu
+        display: flex
+
+        ul
+            list-style: none
+            display: flex
+            align-items: center
+            margin: 0 2.5em
+
+            li a
+                text-decoration: none
+                margin: 0 1em
+                color: hsl(0, 0%, 75%)
+                transition: color .5s ease
+
+                &:hover
+                    color: hsl(260, 8%, 14%)
+
+    .nav-container__title-menu__logo
+        display: flex
+        align-items: center
+
+    .nav-container__buttons
+        button
+            background: none
+            border: none
+            margin: 0 .5em
+            padding: .5em 1.5em
+            color: hsl(0, 0%, 75%)
+            font-size: 1rem
+            border-radius: 30px
+            cursor: pointer
+
+            &:nth-child(2)
+                background-color: hsl(180, 66%, 49%)
+                color: #fff
+
+                &:hover
+                    background-color: hsl(180, 66%, 49%, .5)
+</style>
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,7 +1,17 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+    import Navbar from '../components/navbar.svelte';  
+</script>
 
-<style lang="sass">
-    h1
-        color: red
+<Navbar/>
+
+<style lang="scss">
+@import url('https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,500;0,700;1,500&display=swap');
+
+:global(*) {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+    font-family: 'Poppins', sans-serif;
+}
+
 </style>
